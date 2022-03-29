6158547a15b3b46e957927d434c277e162e3b7df
Thomas Walker <thomasnwalker@yahoo.com>
2022-03-28 17:39:28 -0700

    basic nav toggle

---
+++ assets/index.js
@@ -0,0 +1,39 @@
+let containerEl = document.querySelector("#container");
+let title = document.querySelector("#container-title");
+let content = document.createElement("h1")
+function showAbout(){
+    title.innerHTML = "About";
+  //clear other content before append 
+   content.innerHTML = "";
+
+
+ 
+   // p.style.color = "rgba(255, 255, 255, .8)";
+    content.append("Our mission is to cultivate a healthier community by providing information and services that increase health awareness and functionality within households")
+    content.className = "about";
+    containerEl.append(content);
+}
+
+
+
+function showMembership(){
+    title.innerHTML = "Membership";
+    //clear content first 
+    content.innerHTML = "";
+
+ 
+    content.append("Membership Options Coming soon!")
+    content.className = "membership";
+    containerEl.append(content);
+}
+
+function showContactUs(){
+    title.innerHTML = "Contact Us";
+    //clear content first
+    content.innerHTML = "";
+  
+
+    content.append("Reach us on instragram!")
+    content.className = "contact";
+    containerEl.append(content);
+}
